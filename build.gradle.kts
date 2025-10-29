b87c414cea1908fd9977ba7f962d8166d972b3a0
TALITA RIBEIRO GUEDES <TALITA_4917@uirapuru.br>
2025-10-28 22:15:42 -0300

    AAAAAAAAA

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    // Plugin Kotlin com versão definida
+    // Plugin Kotlin compatível
     kotlin("android") version "1.9.10" apply false
+
+    // Versão do Android Gradle Plugin compatível com o seu Android Studio
     id("com.android.application") version "8.8.0" apply false
     id("com.android.library") version "8.8.0" apply false
 }
